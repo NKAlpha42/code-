9549116c8930caeb4fdd5ceac0a61adef4d0719b
NKAlpha42 <149430342+NKAlpha42@users.noreply.github.com>
2023-12-11 13:50:36 -0700

    Add files via upload

---
+++ MadLibsProject/madlib.js
@@ -0,0 +1,27 @@
+
+function write() {
+    var year = document.getElementById("yearInp").value;
+    var monster = document.getElementById("monsterInp").value;
+    var disaster = document.getElementById("disasterInp").value;
+    var oldfart = document.getElementById("oldfartInp").value;
+    var verb1 = document.getElementById("verb1Inp").value;
+    var verb2 = document.getElementById("verb2Inp").value;
+    var verb3 = document.getElementById("verb3Inp").value;
+    var verb4 = document.getElementById("verb4Inp").value;
+    var verb5 = document.getElementById("verb5Inp").value;
+    var adj1 = document.getElementById("adj1Inp").value;
+    var adj2 = document.getElementById("adj2Inp").value;
+    var adj3 = document.getElementById("adj3Inp").value;
+    var adj4 = document.getElementById("adj4Inp").value;
+    var noun1 = document.getElementById("noun1Inp").value;
+    var noun2 = document.getElementById("noun2Inp").value;
+    var res = document.getElementById("resInp").value;
+    var number = document.getElementById("numberInp").value;
+    var celeb = document.getElementById("celebInp").value;
+
+
+
+
+
+    document.getElementById('story').innerHTML = '<br><br><br><br><br><br><h4>If you’re reading this letter, then you have to act fast. I’m writing this in the year ' + year + ' AD, and I need to warn you about the terrible things that happen. In the future, a ' + disaster + ' destroyed all of Mexico, and everyone there had to move to Brazil. But then the ' + monster + ' army came out of hiding and attacked all of South America!! The other countries thought they were safe, until ' + oldfart + ' came back from the dead and ' + verb1 + ' all across europe! Don’t even get me started on how the ' + adj1 +' ' + noun1 + ' enslaved all the penguins in antarctica. Everything is ' + adj2 + '!! In order to stop this future, you have to follow my instructions carefully. You must go to ' + res + ' and order ' + number + ' of their special ' + adj3 + ' burgers. Then you have to eat them all before the ' + noun2 + ' finds you. Once the ' + noun2 + ' shows up, you’ll have to beat it in a special ' + verb2 + ' battle. If you can manage to win, then the future will be saved! We’re all counting on you so that ' + celeb + ' doesn’t ' + verb3 +'. Oh no! The ' + adj4 + ' army has found me! I don’t have any more time! They’re going to ' + verb4 + ' me until I ' + verb5 + '! Hurry, save us!</h4>';
+}
